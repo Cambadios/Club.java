65e34cb8ba2c741aed93a0da4e45b5d4e73e0f28
Cambadios <115101721+Cambadios@users.noreply.github.com>
2022-10-05 13:26:29 -0400

    Update Campeonato.java

--- Campeonato.java
+++ Campeonato.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Campeonato {
     Judoka[] lista;
-    int llave;
+    int key;
 
     public Campeonato(int llave, Judoka[] lista) {
         this.lista = lista;
